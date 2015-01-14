728195c2a2e003c3e0e219cacb930f96a82bc444
gillchristian <gillchristiang@gmail.com>
2015-01-14 11:05:28 +0600

    Added total cost to both barracks and spells list.

--- scripts/controllers/army.js
+++ scripts/controllers/army.js
@@ -26,43 +26,86 @@ angular.module('clashApp.controllers', [])
             }
         }
         
-        /// Execute services to get data
+        // Execute services to get data
         $scope.services.getSpells();
         $scope.services.getLight();
         $scope.services.getDark();
 
-        $scope.cantidad = function () {
+        $scope.cantidad = function (option) {
             var cantidad = 0;
             var aux = 0;
-            for (var i = $scope.light.length - 1; i >= 0; i--) {
-                aux = $scope.light[i].amount * $scope.light[i].space;
-                cantidad += aux;
-            };
+            switch (option) {
+                case 'light':
+                    for (var i = $scope.light.length - 1; i >= 0; i--) {
+                        aux = $scope.light[i].amount * $scope.light[i].space;
+                        cantidad += aux;
+                    };
+                    break;
+                case 'dark':
+                    for (var i = $scope.dark.length - 1; i >= 0; i--) {
+                        aux = $scope.dark[i].amount * $scope.dark[i].space;
+                        cantidad += aux;
+                    };
+                    break;
+                case 'spells':
+                    for (var i = $scope.spells.length - 1; i >= 0; i--) {
+                        aux = $scope.spells[i].amount * $scope.spells[i].space;
+                        cantidad += aux;
+                    };
+                    break;
+            }
             return cantidad;
         }
 
-        $scope.costo = function () {
+        $scope.costo = function (option) {
             var costo = 0;
             var aux = 0;
             var index = 0;
-            for (var i = $scope.light.length - 1; i >= 0; i--) {
-                index = $scope.light[i].lvl-1;
-                aux = $scope.light[i].cost[index] * $scope.light[i].amount;
-                costo += aux;
-                console.log("index, unit cost, aux, costo");
-                console.log(index);
-                console.log($scope.light[i].cost[index]);
-                console.log(costo);
-                console.log(aux);
-            };
+            switch (option) {
+                case 'light':
+                    for (var i = $scope.light.length - 1; i >= 0; i--) {
+                        index = $scope.light[i].lvl-1;
+                        aux = $scope.light[i].cost[index] * $scope.light[i].amount;
+                        costo += aux;
+                    };
+                    break;
+                case 'dark':
+                    for (var i = $scope.dark.length - 1; i >= 0; i--) {
+                        index = $scope.dark[i].lvl-1;
+                        aux = $scope.dark[i].cost[index] * $scope.dark[i].amount;
+                        costo += aux;
+                    };
+                    break;
+                case 'spells':
+                    for (var i = $scope.spells.length - 1; i >= 0; i--) {
+                        index = $scope.spells[i].lvl-1;
+                        aux = $scope.spells[i].cost[index] * $scope.spells[i].amount;
+                        costo += aux;
+                    };
+                    break;
+            }
             return costo;
         }
 
-        $scope.tiempo = function () {
+        $scope.tiempo = function (option) {
             var tiempo = 0;
-            for (var i = $scope.light.length - 1; i >= 0; i--) {
-                tiempo += ($scope.light[i].time * $scope.light[i].amount);
-            };
+            switch (option) {
+                case 'light':
+                    for (var i = $scope.light.length - 1; i >= 0; i--) {
+                        tiempo += ($scope.light[i].time * $scope.light[i].amount);
+                    };
+                    break;
+                case 'dark':
+                    for (var i = $scope.dark.length - 1; i >= 0; i--) {
+                        tiempo += ($scope.dark[i].time * $scope.dark[i].amount);
+                    };
+                    break;
+                case 'spells':
+                    for (var i = $scope.spells.length - 1; i >= 0; i--) {
+                        tiempo += ($scope.spells[i].time * $scope.spells[i].amount);
+                    };
+                    break;
+            }
             return tiempo;
         }
 
