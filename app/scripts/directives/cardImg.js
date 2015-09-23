e1bb72bb3772e5331e6c0bcfbed331c72f50cbae
gillchristian <gillchristiang@gmail.com>
2015-09-23 00:52:45 -0300

    fix pictures url :sweat:

--- app/scripts/directives/cardImg.js
+++ app/scripts/directives/cardImg.js
@@ -1,8 +1,8 @@
 (function() {
    'use strict';
    angular.module('clashApp.directives')
-      .directive('cardImg', [
-         function () {
+      .directive('cardImg', ['$location',
+         function ($location) {
             return {
                restrict: 'E',
                scope: {
@@ -11,9 +11,8 @@
                templateUrl: 'app/views/templates/cardImg.html',
                link: function (scope, elm, attr) {
                   var u = scope.u;
-                  console.log(attr.baseUrl);
-                  if(scope.u.folder.match(/spell/i) !== null)  scope.src = attr.baseUrl + u.folder + '/' + u.unit + '.png' ; 
-                  else scope.src = attr.baseUrl + u.folder + '/' + u.unit + '-' + u.lvl + '.png';
+                  if(scope.u.folder.match(/spell/i) !== null)  scope.src = "assets/images/" + u.folder + '/' + u.unit + '.png' ; 
+                  else scope.src = "assets/images/" + u.folder + '/' + u.unit + '-' + u.lvl + '.png';
                   }
                }
          }]);
