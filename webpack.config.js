dfe4aae8369476646de18203ccc78bd7c1daa83e
gillchristian <gillchristiang@gmail.com>
2016-02-07 17:25:43 -0300

    reworked webpack workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
+const path = require('path');
+
 // webpack.config.js
 module.exports = {
-  entry: './src/js/index.js',
+  entry: {
+    bundle: './src/js',
+    tests: './src/js/tests/index.js'
+  },
+  output: {
+    path: __dirname + '/build/js',
+    filename: '[name].js'
+  },
   module: {
     loaders: [
       { 
 				test: /\.js$/, 
 				loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
         exclude: [/node_modules/, /app/]
 			}
     ]
